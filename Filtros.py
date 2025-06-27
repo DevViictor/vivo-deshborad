da20a07439757d9b030de6287d8aa9a9e985bd9c
devvictor <victorritossantos@gmail.com>
2025-06-27 15:45:35 -0300

    update

--- Filtros.py
+++ Filtros.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 Select_Coluns = ["DATA" ,"Plano" , "Produto"]
 Select_Coluns2 = ["DATA","Serviço"]
-Select_Coluns3 = ["DATA", "Serviço","Instância"]
+Select_Coluns3 = ["DATA", "Serviço"]
 
 Select_Filtro = "CONTROLE"
 Select_Filtro2 = "PÓS PAGO"
